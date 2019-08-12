c0437accf98345724a511091c035a356c09e16cc
Charles Dunn <spacegrey42@icloud.com>
2019-08-12 10:14:51 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CTest
+//
+//  Created by Charles Dunn on 7/31/19.
+//  Copyright © 2019 Charles Dunn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
